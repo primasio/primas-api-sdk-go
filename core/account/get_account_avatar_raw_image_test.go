015ac8490055860e8966221cc2e12ac57af8f6d8
陈相熔 <chenxiangrong@yuanben.org>
2018-08-06 10:04:00 +0800

    add subaccount avatar test

--- core/account/get_account_avatar_raw_image_test.go
+++ core/account/get_account_avatar_raw_image_test.go
@@ -14,3 +14,16 @@ func TestGetAccountAvatarRawImage(t *testing.T) {
 
 	t.Logf("resultReponse:" + string(resultReponse))
 }
+
+func TestGetSubAccountAvatarRawImage(t *testing.T) {
+	account_id := "809a85f7ddf8ae5aaa49fe30be10e07e09156dc04166fab98bbd7bb42b2dc26c"
+	sub_account_id := "a_0001"
+
+	resultReponse, err := GetSubAccountAvatarRawImage(account_id, sub_account_id)
+	if err != nil {
+		t.Errorf("GetSubAccountAvatarRawImage error:%v", err.Error())
+		return
+	}
+
+	t.Logf("resultReponse:" + string(resultReponse))
+}
