98f46bc956e71da68e995ac704ce847490a73040
陈相熔 <chenxiangrong@yuanben.org>
2018-08-23 11:45:25 +0800

    add account joined groups

---
+++ core/account/get_account_joined_groups.go
@@ -0,0 +1,80 @@
+package account
+
+import (
+	"encoding/json"
+	"errors"
+
+	"github.com/primasio/primas-api-sdk-go/config"
+	"github.com/primasio/primas-api-sdk-go/core"
+	"github.com/primasio/primas-api-sdk-go/core/dtcp/dtcpv1"
+	"github.com/primasio/primas-api-sdk-go/core/tool"
+)
+
+type AccountJoinedGroupsResponse struct {
+	core.Response
+	Data []dtcpv1.GroupGet `json:"data"`
+}
+
+func GetAccountJoinedGroups(account_id string, page, pageSize int) (*AccountJoinedGroupsResponse, error) {
+	if account_id == "" {
+		return nil, errors.New("account_id is empty")
+	}
+
+	url := config.CONST_Server + `/accounts/` + account_id + `/joined/groups`
+	queryParams := make(map[string]interface{}, 0)
+	if page > 0 {
+		queryParams["page"] = page
+	}
+	if pageSize > 0 {
+		queryParams["page_size"] = pageSize
+	}
+
+	response, err := tool.Http_Get(url, queryParams)
+	if err != nil {
+		return nil, err
+	}
+
+	var responseObj AccountJoinedGroupsResponse
+	err = json.Unmarshal(response, &responseObj)
+	if err != nil {
+		return nil, err
+	}
+
+	if responseObj.ResultCode != core.CONST_ResultCode_Success {
+		return nil, errors.New(responseObj.ResultMsg)
+	}
+
+	return &responseObj, nil
+}
+
+func GetSubAccountJoinedGroups(account_id, sub_account_id string, page, pageSize int) (*AccountJoinedGroupsResponse, error) {
+	if account_id == "" {
+		return nil, errors.New("account_id is empty")
+	}
+
+	url := config.CONST_Server + `/accounts/` + account_id + `/sub/` + sub_account_id + `/joined/groups`
+	queryParams := make(map[string]interface{}, 0)
+	if page > 0 {
+		queryParams["page"] = page
+	}
+	if pageSize > 0 {
+		queryParams["page_size"] = pageSize
+	}
+
+	response, err := tool.Http_Get(url, queryParams)
+	if err != nil {
+		return nil, err
+	}
+
+	var responseObj AccountJoinedGroupsResponse
+	err = json.Unmarshal(response, &responseObj)
+	if err != nil {
+		return nil, err
+	}
+
+	if responseObj.ResultCode != core.CONST_ResultCode_Success {
+		return nil, errors.New(responseObj.ResultMsg)
+	}
+
+	return &responseObj, nil
+}
---
+++ core/account/get_account_joined_groups_test.go
@@ -0,0 +1,54 @@
+package account
+
+import (
+	"testing"
+
+	"github.com/primasio/primas-api-sdk-go/core"
+)
+
+func TestGetAccountJoinedGroups(t *testing.T) {
+	account_id := "809a85f7ddf8ae5aaa49fe30be10e07e09156dc04166fab98bbd7bb42b2dc26c"
+	resultGetAccountJoinedGroups, err := GetAccountJoinedGroups(account_id, 0, 20)
+	if err != nil {
+		t.Errorf("GetAccountJoinedGroups error:%v", err.Error())
+		return
+	}
+
+	if resultGetAccountJoinedGroups != nil {
+		t.Logf("GetAccountJoinedGroups response value:%v", resultGetAccountJoinedGroups)
+		if resultGetAccountJoinedGroups.ResultCode != core.CONST_ResultCode_Success {
+			t.Errorf("GetAccountJoinedGroups response error:%v", resultGetAccountJoinedGroups.ResultMsg)
+			return
+		}
+
+		if resultGetAccountJoinedGroups.Data != nil {
+			t.Logf("GetAccountJoinedGroups response AccountTokensData value:%#v", resultGetAccountJoinedGroups.Data)
+		} else {
+			t.Logf("GetAccountJoinedGroups response AccountTokensData value don't find ")
+		}
+	}
+}
+
+func TestGetSubAccountJoinedGroups(t *testing.T) {
+	account_id := "809a85f7ddf8ae5aaa49fe30be10e07e09156dc04166fab98bbd7bb42b2dc26c"
+	sub_account_id := "a_0001"
+	resultGetSubAccountJoinedGroups, err := GetSubAccountJoinedGroups(account_id, sub_account_id, 0, 20)
+	if err != nil {
+		t.Errorf("GetSubAccountJoinedGroups error:%v", err.Error())
+		return
+	}
+
+	if resultGetSubAccountJoinedGroups != nil {
+		t.Logf("GetSubAccountJoinedGroups response value:%v", resultGetSubAccountJoinedGroups)
+		if resultGetSubAccountJoinedGroups.ResultCode != core.CONST_ResultCode_Success {
+			t.Errorf("GetSubAccountJoinedGroups response error:%v", resultGetSubAccountJoinedGroups.ResultMsg)
+			return
+		}
+
+		if resultGetSubAccountJoinedGroups.Data != nil {
+			t.Logf("GetSubAccountJoinedGroups response AccountTokensData value:%#v", resultGetSubAccountJoinedGroups.Data)
+		} else {
+			t.Logf("GetSubAccountJoinedGroups response AccountTokensData value don't find ")
+		}
+	}
+}
